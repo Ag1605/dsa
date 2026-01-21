ab151d1ed518b366ef95752a64dc4fcfb56a9218
Akshat <akshatgoyal96@gmail.com>
2026-01-21 12:23:04 +0530

    new

---
+++ array/find k largest element .cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int findKthLargest(vector<int>& arr, int k) {
+    int n = arr.size();
+    nth_element(arr.begin(), arr.begin() + (n - k), arr.end());
+    return arr[n - k];
+}
+
+
+// nth element dont sort the array but it puts the kth largest element at the correct position
+
+
+
+int main() {
+    vector<int> arr = {3, 2, 1, 5, 6, 4};
+    int k = 2;
+    cout << findKthLargest(arr, k);
+}
