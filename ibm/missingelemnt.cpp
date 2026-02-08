c7b145795117286aae772c468bc95cf72a535a1d
Akshat <akshatgoyal96@gmail.com>
2026-02-08 11:49:02 +0530

    new

--- ibm/missingelemnt.cpp
+++ ibm/missingelemnt.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int findMissing(int arr[], int n) {
     int xor1 = 0;   // XOR of array elements
-    int xor2 = 0;   // XOR of numbers from 1 to n+1
+    int xor2 = 0;   // XOR of numbers from 1 to n+1  a\
 
     // XOR all array elements
     for (int i = 0; i < n; i++) {
@@ -23,6 +23,7 @@ int main() {
     int arr[] = {3, 7, 1, 2, 8, 4, 5};
     int n = sizeof(arr) / sizeof(arr[0]);
 
-    cout << "Missing Element: " << findMissing(arr, n);
+     cout << "Missing Element: " << findMissing(arr, n);
+     cout << "Missing Element: " << findMissing(arr, n);
     return 0;
 }
