5ecd7bddcb5ba362765165c0765423ed593b75d7
Akshat Goel <akshatgoyal96@gmail.com>
2026-07-02 08:59:36 +0530

    hhh

---
+++ pattern/binary search/findmininsortedarray.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int findMin(vector<int> &nums)
+    {
+
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left < right)
+        {
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[right])
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid;
+            }
+        }
+
+        return nums[left];
+    }
+};
+
+int main()
+{
+    Solution obj;
+
+    int n;
+    cin >> n;
+
+    vector<int> nums(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
+
+    cout << obj.findMin(nums);
+
+    return 0;
+}
