84da6a5ba4c6d97306c60fff705c097861c0dc4b
Akshat Goel <akshatgoyal96@gmail.com>
2026-07-30 15:32:33 +0530

    hhh

---
+++ heap/heapcodes/heapsort.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Heapify a subtree rooted at index i
+void heapify(vector<int> &arr, int n, int i)
+{
+    int largest = i;
+
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    if (largest != i)
+    {
+        swap(arr[i], arr[largest]);
+
+        heapify(arr, n, largest);
+    }
+}
+
+void heapSort(vector<int> &arr)
+{
+    int n = arr.size();
+
+    // Build Max Heap
+    for (int i = n / 2 - 1; i >= 0; i--)
+    {
+        heapify(arr, n, i);
+    }
+
+    // One by one extract maximum
+    for (int i = n - 1; i > 0; i--)
+    {
+        swap(arr[0], arr[i]);
+
+        heapify(arr, i, 0);
+    }
+}
+
+int main()
+{
+    vector<int> arr = {4, 10, 3, 5, 1};
+
+    heapSort(arr);
+
+    cout << "Sorted Array : ";
+
+    for (int x : arr)
+        cout << x << " ";
+
+    return 0;
+}
